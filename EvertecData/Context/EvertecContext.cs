5ee3a2b8c3cd603dde3762f84a6534080ef69271
sami0705 <samihepa84@gmail.com>
2023-04-02 20:35:29 -0500

    API Evertec Base
    API REST Base Evertec con clase Person

---
+++ EvertecData/Context/EvertecContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using EvertecData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EvertecData.Context
+{
+    public partial class EvertecContext : DbContext
+    {
+        public EvertecContext()
+        {
+        }
+
+        public EvertecContext(DbContextOptions<EvertecContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost; Database=PruebaEvertec; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasKey(e => e.IdPerson);
+
+                entity.ToTable("Person");
+
+                entity.Property(e => e.IdPerson).HasColumnName("id_person");
+
+                entity.Property(e => e.Birthdate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("birthdate");
+
+                entity.Property(e => e.HaveBrothers).HasColumnName("have_brothers");
+
+                entity.Property(e => e.IdMaritalStatus).HasColumnName("id_marital_status");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Photo).HasColumnName("photo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EvertecData/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EvertecData.Models
+{
+    public partial class Person
+    {
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public byte[] Photo { get; set; }
+        public int IdMaritalStatus { get; set; }
+        public bool HaveBrothers { get; set; }
+    }
+}
